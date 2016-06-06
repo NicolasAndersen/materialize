8f70402c53611e893fbf566d29a95788089df8fa
zendkofy <tien.uet.qh2011@gmail.com>
2016-06-06 19:03:56 +0700

    test-2

--- zdk-dist/js/components/buttons.js
+++ zdk-dist/js/components/buttons.js
@@ -5,20 +5,20 @@
     $.fn.reverse = [].reverse;
 
     // Hover behaviour: make sure this doesn't work on .click-to-toggle FABs!
-    $(document).on('mouseenter.fixedActionBtn', '.fixed-action-btn:not(.click-to-toggle)', function(e) {
+    $(document).on('mouseenter.fixedActionBtn', '.zm-fixed-action-btn:not(.click-to-toggle)', function(e) {
       var $this = $(this);
       openFABMenu($this);
     });
-    $(document).on('mouseleave.fixedActionBtn', '.fixed-action-btn:not(.click-to-toggle)', function(e) {
+    $(document).on('mouseleave.fixedActionBtn', '.zm-fixed-action-btn:not(.click-to-toggle)', function(e) {
       var $this = $(this);
       closeFABMenu($this);
     });
 
     // Toggle-on-click behaviour.
-    $(document).on('click.fixedActionBtn', '.fixed-action-btn.click-to-toggle > a', function(e) {
+    $(document).on('click.fixedActionBtn', '.zm-fixed-action-btn.click-to-toggle > a', function(e) {
       var $this = $(this);
       var $menu = $this.parent();
-      if ($menu.hasClass('active')) {
+      if ($menu.hasClass('zm-active')) {
         closeFABMenu($menu);
       } else {
         openFABMenu($menu);
@@ -39,10 +39,10 @@
 
   var openFABMenu = function (btn) {
     $this = btn;
-    if ($this.hasClass('active') === false) {
+    if ($this.hasClass('zm-active') === false) {
 
       // Get direction option
-      var horizontal = $this.hasClass('horizontal');
+      var horizontal = $this.hasClass('zm-horizontal');
       var offsetY, offsetX;
 
       if (horizontal === true) {
@@ -51,13 +51,13 @@
         offsetY = 40;
       }
 
-      $this.addClass('active');
-      $this.find('ul .btn-floating').velocity(
+      $this.addClass('zm-active');
+      $this.find('ul .zm-btn-floating').velocity(
         { scaleY: ".4", scaleX: ".4", translateY: offsetY + 'px', translateX: offsetX + 'px'},
         { duration: 0 });
 
       var time = 0;
-      $this.find('ul .btn-floating').reverse().each( function () {
+      $this.find('ul .zm-btn-floating').reverse().each( function () {
         $(this).velocity(
           { opacity: "1", scaleX: "1", scaleY: "1", translateY: "0", translateX: '0'},
           { duration: 80, delay: time });
@@ -69,7 +69,7 @@
   var closeFABMenu = function (btn) {
     $this = btn;
     // Get direction option
-    var horizontal = $this.hasClass('horizontal');
+    var horizontal = $this.hasClass('zm-horizontal');
     var offsetY, offsetX;
 
     if (horizontal === true) {
@@ -78,14 +78,12 @@
       offsetY = 40;
     }
 
-    $this.removeClass('active');
+    $this.removeClass('zm-active');
     var time = 0;
-    $this.find('ul .btn-floating').velocity("stop", true);
-    $this.find('ul .btn-floating').velocity(
+    $this.find('ul .zm-btn-floating').velocity("stop", true);
+    $this.find('ul .zm-btn-floating').velocity(
       { opacity: "0", scaleX: ".4", scaleY: ".4", translateY: offsetY + 'px', translateX: offsetX + 'px'},
       { duration: 80 }
     );
   };
-
-
 }( jQuery ));
