bc0a5c7d1521cc156fa5c660558aece53c45e338
zendkofy <tien.uet.qh2011@gmail.com>
2016-06-06 17:41:10 +0700

    test-2

--- zdk-dist/js/require-config.js
+++ zdk-dist/js/require-config.js
@@ -9,16 +9,21 @@ require.config({
         }
     },
     paths: {
-        jquery: 'utils/jquery-1.9.1.min',
-        velocity: 'utils/velocity.min',
+        'jquery': 'utils/jquery/jquery-1.9.1.min',
+        'jquery/ui': 'utils/jquery/jquery-ui',
+        'jquery/ui/widget': 'utils/jquery/jquery.widget',
+        'velocity': 'utils/jquery/velocity.min',
+        'zdkmaterial/groupButton': 'widget/group-button',
         'zdkmaterial/buttons': 'components/buttons',
         'zdkmaterial/waves': 'components/waves'
     },
     shim: {
-        velocity: ['jquery'],
+        'velocity': ['jquery'],
+        'jquery/ui': ['jquery'],
         'zdkmaterial/buttons': ['jquery', 'velocity'],
         'zdkmaterial/waves': {
             'exports': 'Waves'
-        }
+        },
+        'zdkmaterial/groupButton': ['jquery', 'jquery/ui/widget']
     }
 });
