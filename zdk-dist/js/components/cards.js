b28c3d94639764654352f582ccf080814ce2ec77
zendkofy <tien.uet.qh2011@gmail.com>
2016-06-15 17:12:10 +0700

    cards

--- zdk-dist/js/components/cards.js
+++ zdk-dist/js/components/cards.js
@@ -9,44 +9,29 @@
         factory(jQuery, Zendkofy);
     }
 }(function ($, $z) {
-    var cardSelector = $z.utils.selectorClass('card'),
-        cardRevealSelector = $z.utils.selectorClass('card-reveal'),
-        activatorSelector = $z.utils.selectorClass(['card', 'activator'], ' '),
-        cardTitleSelector = $z.utils.classnames(['card', 'card-title'], ' ');
-    
-    $(document).ready(function () {
+    $(document).ready(function() {
 
-        $(document).on('click.card', cardSelector, function (e) {
-            if ($(this).find('> ' + cardRevealSelector).length) {
-                if ($(e.target).is($(cardTitleSelector)) || $(e.target).is($(cardTitleSelector + ' i'))) {
+        $(document).on('click.card', '[data-materialize-init=true].zm-card', function (e) {
+            if ($(this).find('> .zm-card-reveal').length) {
+                if ($(e.target).is($('.zm-card-reveal .zm-card-title')) || $(e.target).is($('.zm-card-reveal .zm-card-title i'))) {
                     // Make Reveal animate down and display none
-                    $(this).find(cardRevealSelector)
-                        .velocity({translateY: 0}, {
-                                duration: 225,
-                                queue: false,
-                                easing: 'easeInOutQuad',
-                                complete: function () {
-                                    $(this).css({display: 'none'});
-                                }
-                            }
-                        );
-                }
-                else if ($(e.target).is($(activatorSelector)) ||
-                    $(e.target).is($(activatorSelector + ' i'))) {
-                    $(e.target).closest(cardSelector).css('overflow', 'hidden');
-                    $(this).find(cardRevealSelector)
-                        .css({display: 'block'})
-                        .velocity("stop", false)
-                        .velocity({translateY: '-100%'}, {
-                            duration: 300,
+                    $(this).find('.zm-card-reveal').velocity(
+                        {translateY: 0}, {
+                            duration: 225,
                             queue: false,
-                            easing: 'easeInOutQuad'
-                        });
+                            easing: 'easeInOutQuad',
+                            complete: function() { $(this).css({ display: 'none'}); }
+                        }
+                    );
+                }
+                else if ($(e.target).is($('.zm-card .zm-activator')) ||
+                    $(e.target).is($('.zm-card .zm-activator i')) ) {
+                    $(e.target).closest('.zm-card').css('overflow', 'hidden');
+                    $(this).find('.zm-card-reveal').css({ display: 'block'}).velocity("stop", false).velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeInOutQuad'});
                 }
             }
 
-            $('.card-reveal').closest('.card').css('overflow', 'hidden');
-
+            $('.zm-card-reveal').closest('.zm-card').css('overflow', 'hidden');
         });
 
     });
